456dee695090f8c745474e269307897a2629f9b4
Mesut <mesutdogansoy@gmail.com>
2021-04-08 17:40:14 +0300

    resolve var keyword

--- src/main/java/com/mesut/backporter/MyVisitor.java
+++ src/main/java/com/mesut/backporter/MyVisitor.java
@@ -1,13 +1,30 @@
 package com.mesut.backporter;
 
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.*;
 
 public class MyVisitor extends ASTVisitor {
 
     @Override
-    public boolean visit(VariableDeclarationExpression node) {
+    public boolean visit(VariableDeclarationStatement node) {
+        System.out.println("varrrr");
+        if (node.getType().isVar()) {
+            ITypeBinding binding = node.getType().resolveBinding();
+            if (binding == null) {
+                System.out.println("null type " + node);
+            }
+            else {
+                node.setType(makeType(binding, node.getAST()));
+            }
+        }
         return super.visit(node);
+    }
+
+    Type makeType(ITypeBinding binding, AST ast) {
+        if (binding.isArray()) {
+            return ast.newArrayType(makeType(binding.getElementType(), ast), binding.getDimensions());
+        }
+        else {
+            return ast.newSimpleType(ast.newSimpleName(binding.getName()));
+        }
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,7 +3,8 @@ import com.mesut.backporter.Porter;
 public class Test {
     @org.junit.Test
     public void name() {
-        String src = "/media/mesut/SSD-DATA/IdeaProjects/jls/src/main/java";
+        //String src = "/media/mesut/SSD-DATA/IdeaProjects/jls/src/main/java";
+        String src = "/home/mesut/Desktop/IdeaProjects/backporter/src/test/resources";
         String dest = "/home/mesut/Desktop/port";
 
         Porter porter = new Porter(src, dest);
